56f1d7bee35f2892f2b27d4c47375984f1ed568a
C0DEFLUX <niksklavins04@gmail.com>
2024-11-21 18:52:34 +0200

    Added commission employee

--- salary-sys/src/EmployeeTypes/CommissionEmployee.java
+++ salary-sys/src/EmployeeTypes/CommissionEmployee.java
@@ -1,4 +1,26 @@
 package EmployeeTypes;
 
-public class CommissionEmployee {
+public class CommissionEmployee extends Employee
+{
+    private double commissionRate;
+    private double sales;
+
+    public CommissionEmployee(String firstName, String lastName, String socialNumber, String contractType, double commissionRate, double sales)
+    {
+        super(firstName, lastName, socialNumber, contractType);
+        this.commissionRate = commissionRate;
+        this.sales = sales;
+    }
+
+    @Override
+    public double income()
+    {
+        return commissionRate * sales;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + ", Commission Rate: " + commissionRate + ", Sales: " + sales;
+    }
 }
