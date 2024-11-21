9c818900393295f7222fab6d08e0caf49e375edc
C0DEFLUX <niksklavins04@gmail.com>
2024-11-21 18:48:35 +0200

    Added code for part time employee

--- salary-sys/src/EmployeeTypes/PartTimeEmployee.java
+++ salary-sys/src/EmployeeTypes/PartTimeEmployee.java
@@ -1,4 +1,26 @@
 package EmployeeTypes;
 
-public class PartTimeEmployee {
+public class PartTimeEmployee extends Employee
+{
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(String firstName, String lastName, String socialNumber, String contractType, double HourlyRate, int hoursWorked)
+    {
+         super(firstName, lastName, socialNumber, contractType);
+         this.hourlyRate = HourlyRate;
+         this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double income()
+    {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + ", Hourly Rate: " + hourlyRate + ", Hours Worked: " + hoursWorked;
+    }
 }
